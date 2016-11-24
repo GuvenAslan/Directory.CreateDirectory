795a1413ec21c4541bf25848d764541ba765c2f9
guven <guvennaslan@gmail.com>
2016-11-25 00:03:07 +0200

    Completed

---
+++ Directory.CreateDirectory/Directory.CreateDirectory/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directory.CreateDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"c:\testFolder";
+
+            string subPath = System.IO.Path.Combine(folderPath, "subFolder");
+
+            System.IO.Directory.CreateDirectory(subPath);
+
+            string fileName = System.IO.Path.GetRandomFileName();
+
+            subPath = System.IO.Path.Combine(subPath, fileName);
+
+            if (!System.IO.File.Exists(subPath))
+            {
+                using (System.IO.FileStream fileStream = System.IO.File.Create(subPath))
+                {
+                    for (byte i = 0; i < 100; i++)
+                    {
+                        fileStream.WriteByte(i);
+                    }
+                }
+            }
+
+            try
+            {
+                byte[] readBuffer = System.IO.File.ReadAllBytes(subPath);
+                foreach (byte _byte in readBuffer)
+                {
+                    Console.WriteLine(_byte);
+                }
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
